ff48c09fc45c38e781053e638aeb56fc0525c376
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-02-06 14:17:52 -0300

    Se completa educación y experiencia

--- AngularPortfolio/src/app/components/about/about.component.ts
+++ AngularPortfolio/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  miPortfolio:any;
+  constructor(private datosPortfolio:PortfolioService ) { }
 
   ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.miPortfolio=data;
+    });
   }
 
 }
