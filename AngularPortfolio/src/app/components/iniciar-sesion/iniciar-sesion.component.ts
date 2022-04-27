196767406bdff23750ce2ffab5615d395ae6a1c1
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-04-27 12:34:20 -0300

    intento de inicio de sesion

--- AngularPortfolio/src/app/components/iniciar-sesion/iniciar-sesion.component.ts
+++ AngularPortfolio/src/app/components/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AutenticacionService } from 'src/app/services/autenticacion.service';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -9,7 +11,7 @@ import { EmailValidator, FormBuilder, FormGroup, Validators } from '@angular/for
 export class IniciarSesionComponent implements OnInit {
   form:FormGroup;
 
-  constructor(private formBuilder:FormBuilder ) { 
+  constructor(private formBuilder:FormBuilder, private autenticacionService:AutenticacionService, private ruta:Router) { 
     this.form=this.formBuilder.group(
       {
         email:['',[Validators.required,Validators.email]],
@@ -34,4 +36,15 @@ export class IniciarSesionComponent implements OnInit {
   get Password(){
     return this.form.get('password');
   }
+
+  onEnviar(event:Event){
+    event.preventDefault;
+    this.autenticacionService.IniciarSesion(this.form.value).subscribe(data=>{
+      console.log("DATA" + JSON.stringify(data));
+      this.ruta.navigate(['/portfolio']);
+
+    })
+  }
+
+
 }
--- AngularPortfolio/src/app/services/portfolio.service.ts
+++ AngularPortfolio/src/app/services/portfolio.service.ts
@@ -6,11 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PortfolioService {
+  //url:string="http://npinti.ddns.net:9008/api/";
 
   constructor(private http:HttpClient) { }
 
   obtenerDatos():Observable<any>{
     console.log("El servicio portfolio esta corriendo")
-    return this.http.get('./assets/data/data.json');
+    return this.http.get<any>('./assets/data/data.json');
+    //return this.http.get<any>('this.url+"persona"'); ESTA DEVOLVERIA SOLO LOS DATOS DE PERSONA
+    //Aca todavia no llegan los datos del servidor, porque el servidor esta esperando el token en el encabezadp
+    //del request, el cual no esta llegando
   }
 }
