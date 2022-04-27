0f3a0bd438ce4d4e6d571833ae1f5024a545922e
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-04-27 12:35:49 -0300

    Se crea initializr

---
+++ backendPortfolio/src/main/java/com/florencianionquepan/backendPortfolio/BackendPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.florencianionquepan.backendPortfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPortfolioApplication.class, args);
+	}
+
+}
