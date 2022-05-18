85ea3fa21f2e61a03d9d177b0e071883ed0ec7ac
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-05-18 16:19:29 -0300

    Se agrega el component de tecnología

---
+++ AngularPortfolio/src/app/components/technology/technology.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
+import { faPlus }from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-technology',
+  templateUrl: './technology.component.html',
+  styleUrls: ['./technology.component.css']
+})
+export class TechnologyComponent implements OnInit {
+
+  faPlus = faPlus;
+  tecnologiaLista:any;
+  constructor(private datosPortfolio:PortfolioService ) { }
+
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      this.tecnologiaLista=data.tecnologia;
+    });
+  }
+}
