e1fbd021adb6cc19b429ac5e80f2be6ecbf352e5
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-07-06 18:35:03 -0300

    actualizo el componente about
    Se une el componente about con el backend

--- AngularPortfolio/src/app/components/about/about.component.ts
+++ AngularPortfolio/src/app/components/about/about.component.ts
@@ -1,22 +1,37 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/services/portfolio.service';
 import { faUserEdit }from '@fortawesome/free-solid-svg-icons';
+import { Persona } from 'src/app/models/persona';
+import { HeaderService } from 'src/app/services/header.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
-export class AboutComponent implements OnInit {
+
+export class AboutComponent{
   faUserEdit = faUserEdit;
-  miPortfolio:any;
-  constructor(private datosPortfolio:PortfolioService ) { }
+
+  public Persona:any | undefined;
+  public editPersona:Persona | undefined;
+
+  constructor (private headerService:HeaderService){}
 
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos().subscribe(data =>{
-      console.log(data);
-      this.miPortfolio=data;
-    });
+    this.verPersona();
+  }
+
+  public verPersona():void{
+    this.headerService.verPersona().subscribe({
+      next:(response:any)=>{
+        this.Persona=response[0];
+        console.log(this.Persona);
+      },
+      error:(error:HttpErrorResponse)=>{
+        console.log(error.message);
+      }
+    })
   }
 
 }
