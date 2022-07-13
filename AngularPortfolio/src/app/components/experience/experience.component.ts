6043cb2ef13db374021ccfbce4050c19875e6577
florencianionquepan <85314154+florencianionquepan@users.noreply.github.com>
2022-07-13 11:35:00 -0300

    Agrega modals a experiencia

--- AngularPortfolio/src/app/components/experience/experience.component.ts
+++ AngularPortfolio/src/app/components/experience/experience.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { PortfolioService } from 'src/app/services/portfolio.service';
 import { faTrashAlt , faPlus,faPen } from '@fortawesome/free-solid-svg-icons';
+import { ExperienceService } from 'src/app/services/experience.service';
+import { Experiencia } from 'src/app/models/experiencia';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-experience',
@@ -13,25 +16,85 @@ export class ExperienceComponent implements OnInit {
   faPen=faPen;
   faTrash= faTrashAlt;
 
-  experienciaLista:any;
-  constructor(private datosPortfolio:PortfolioService) { }
+  public experienciaLista:Experiencia[]=[];
+  public modificarExperiencia:Experiencia | undefined;
+  public eliminarExperiencia:Experiencia | undefined;
+
+  constructor(private experienciaService:ExperienceService) { }
 
   ngOnInit(): void {
-    this.datosPortfolio.obtenerDatos().subscribe(data =>{
-      this.experienciaLista=data.experienciaLaboral;
+    this.verExperiencia();
+  }
+  
+  
+  public verExperiencia():void{
+    this.experienciaService.verExperiencia().subscribe({
+      next:(Response:Experiencia[])=>{
+        this.experienciaLista=Response;
+      },
+      error:(error:HttpErrorResponse)=>{
+        console.log(error.message);
+      }
+      
     })
   }
 
-  sumarExperiencia(experienciaLista:any){
-    console.log(experienciaLista);
+  public onOpenModal(mode:String, experiencia?:Experiencia):void{
+    const container=document.querySelector('#main-component');
+    const button=document.createElement('button');
+    button.style.display='none';
+    button.setAttribute('data-toggle', 'modal'); 
+    if(mode==='agregar'){
+      button.setAttribute('data-toggle', '#agregarExperienciaModal');
+    }else if(mode==='eliminar'){
+      this.eliminarExperiencia=experiencia;
+      button.setAttribute('data-toggle', '#eliminarExperienciaModal');
+    }else if(mode=='modificar'){
+      this.modificarExperiencia=experiencia;
+      button.setAttribute('data-toggle', '#modificarExperienciaModal');
+    }
+    container?.appendChild(button);
+    button.click;
   }
 
-  editExperiencia(experienciaLista:any){
-    console.log(experienciaLista);
+  public onAgregarExperiencia(addForm:NgForm){
+    document.getElementById('agregar-experiencia-form')?.click();
+    this.experienciaService.agregarExperiencia(addForm.value).subscribe({
+      next:(response:Experiencia)=>{
+        console.log(response);
+        this.verExperiencia();
+        addForm.reset();
+      },
+      error:(error:HttpErrorResponse)=>{
+        console.log(error.message);
+        addForm.reset();
+      }
+    })
   }
 
-  borrarExperiencia(experienciaLista:any){
-    console.log(experienciaLista);
+  public onModificarExperiencia(experiencia:Experiencia){
+    this.modificarExperiencia=experiencia;
+    this.experienciaService.modificarExperiencia(experiencia).subscribe({
+      next:(response:Experiencia)=>{
+        console.log(response);
+        this.verExperiencia();
+      },
+      error:(error:HttpErrorResponse)=>{
+        console.log(error.message);
+      }
+    })
+  }
+
+  public onEliminarExperiencia(idExp:number):void{
+    this.experienciaService.borrarExperiencia(idExp).subscribe({
+      next:(response:void)=>{
+        console.log(response);
+        this.verExperiencia();
+      },
+      error:(error:HttpErrorResponse)=>{
+        console.log(error.message);
+      }
+    })
   }
 
 }
